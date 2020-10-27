8b6d02bbf3efb3575c22fe13cfa19cbcfcd8d3bf
hwld <0701hw@gmail.com>
2020-10-27 16:46:38 +0900

    add styled-components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,26 @@
 import { AppProps } from "next/dist/next-server/lib/router/router";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
 import "../styles/globals.css";
 
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box
+  }
+`;
+
+const theme = {};
+
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 };
 
 export default MyApp;
