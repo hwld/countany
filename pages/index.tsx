f36ac133f24a74bb07ff43878ac4c4109c63cbad
hwld <0701hw@gmail.com>
2020-11-17 16:28:57 +0900

    Change: move CounterContainer to index

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import styled from "styled-components";
 import { Header } from "../components/Header";
 import { Main } from "../components/Main";
-import { CounterContainer } from "../components/CounterContainer";
 import { AddCounterButton } from "../components/AddCounterButton";
 import { useCounters } from "../util/hooks";
 import { getSession } from "next-auth/client";
 import { GetServerSideProps, NextPage } from "next";
+import { AnimatePresence, motion } from "framer-motion";
+import { Counter } from "../components/Counter";
 
 // サーバーサイドでsessionをProviderに入れて、すでにログインされているときにクライアント側でundefinedを返さないようにする
 export const getServerSideProps: GetServerSideProps = async ({ req }) => {
@@ -29,14 +30,30 @@ const Home: NextPage<{ className?: string }> = ({ className }) => {
     <div className={className}>
       <Header />
       <Main>
-        <CounterContainer
-          counters={counters}
-          onEditCounter={editCounter}
-          onRemoveCounter={removeCounter}
-          onCountUp={countUp}
-          onCountDown={countDown}
-          onResetCount={resetCount}
-        />
+        <div className="counterContainer">
+          <AnimatePresence>
+            {counters.map((counter) => (
+              <motion.div
+                key={counter.id}
+                layout
+                initial={{ opacity: 0, scale: 0.5 }}
+                animate={{ opacity: 1, scale: 1 }}
+                exit={{ opacity: 0, scale: 0 }}
+                transition={{ duration: 0.2 }}
+              >
+                <Counter
+                  className="counter"
+                  counter={counter}
+                  onEditCounter={editCounter}
+                  onRemoveCounter={removeCounter}
+                  onCountUp={countUp}
+                  onCountDown={countDown}
+                  onResetCount={resetCount}
+                ></Counter>
+              </motion.div>
+            ))}
+          </AnimatePresence>
+        </div>
       </Main>
       <AddCounterButton onAddCounter={addCounter} className="addCounter" />
     </div>
@@ -48,6 +65,16 @@ const StyledHome = styled(Home)`
   background-color: ${(props) => {
     return props.theme.palette.background.default;
   }};
+
+  & .counterContainer {
+    margin: 20px 100px 0 20px;
+    display: flex;
+    flex-wrap: wrap;
+
+    & > div {
+      margin: 5px;
+    }
+  }
 
   & .addCounter {
     position: absolute;
