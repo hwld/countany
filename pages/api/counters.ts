e58537fca2f2071d8dd306c227cacda035d4d2d6
hwld <0701hw@gmail.com>
2020-11-17 16:22:55 +0900

    Change: empty array if no session exist.

--- pages/api/counters.ts
+++ pages/api/counters.ts
@@ -6,7 +6,12 @@ const prisma = new PrismaClient();
 
 const countersHandler: NextApiHandler = async (req, res) => {
   // ユーザ情報は返さない
-  const session = await getSession();
+  const session = await getSession({ req });
+
+  if (!session) {
+    res.json([]);
+    return;
+  }
 
   const counters = await prisma.counter.findMany({
     where: { user: { email: session?.user.email } },
