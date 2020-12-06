05b177462295e31f3aaf3add68aee902fdf12a1b
hwld <0701hw@gmail.com>
2020-12-06 15:29:05 +0900

    change

--- pages/api/counters.ts
+++ pages/api/counters.ts
@@ -2,12 +2,9 @@ import { NextApiHandler } from "next";
 import { getSession } from "next-auth/client";
 
 const countersHandler: NextApiHandler = async (req, res) => {
-  // ユーザ情報は返さない
   const session = await getSession({ req });
   if (!session) {
-    res.statusCode = 403;
-    res.end("セッションが存在しません。");
-    return;
+    return res.json([]);
   }
 
   res.json([]);
